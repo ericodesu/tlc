251556618aea775cae92accf3f6a87761b31cd32
pepeyen <erickfcaldeira@gmail.com>
2021-06-09 00:01:02 -0300

    Introduced question confirmation method

--- include/services/prompt_handler.h
+++ include/services/prompt_handler.h
@@ -1,12 +1,32 @@
 #ifndef TLC_INCLUDE_PROMPT_HANDLER_H
 #define TLC_INCLUDE_PROMPT_HANDLER_H
 
+#include <functional>
 #include <string>
-#include <algorithm>
+#include <iostream>
 
 namespace prompthandler {
-    bool ShowTryAgain(std::string tryAgainMessage);
+    bool ShowTryAgain(std::string try_again_message);
     void ShowMenu();
+    template<class T>
+    T ShowQuestion(
+        std::string question_message,
+        std::string error_message,
+        std::function<bool (T)> conditional
+    ){
+        T user_input;
+
+        do{
+            std::cout << question_message << ": ";
+            std::cin >> user_input;
+
+            if(conditional(user_input) == false){
+                std::cout << error_message << "\n";
+            }
+        }while(conditional(user_input) == false);
+
+        return user_input;
+    }
 };
 
 #endif
