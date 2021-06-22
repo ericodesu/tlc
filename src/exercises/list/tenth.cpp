8c1100fd766fd1a0c021cdc1090f87c0aba43fab
pepeyen <erickfcaldeira@gmail.com>
2021-06-22 11:46:38 -0300

    Started implementing thenth exercise

--- src/exercises/list/tenth.cpp
+++ src/exercises/list/tenth.cpp
@@ -1,7 +1,23 @@
 #include <exercises/list/tenth.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 #include <iostream>
+#include <string>
 
 void TenthExercise::ExecQ1(){
-    std::cout << "huh";
+    std::string image_path = "Resources/images/Pedestrian.png";
+
+    cv::samples::findFile(image_path);
+
+    cv::Mat pedestrian_image = cv::imread(image_path, cv::IMREAD_COLOR);
+
+    if(pedestrian_image.empty() == false){
+        cv::imshow("Display window", pedestrian_image);
+
+        int user_key = cv::waitKey(0);
+    }else{
+        std::cout << "Could not read the image: " << image_path;
+    }
 };
