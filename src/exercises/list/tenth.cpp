96d8135af16e269436754760836c0c4ea1b2455e
pepeyen <erickfcaldeira@gmail.com>
2021-06-23 09:24:06 -0300

    Implemented the tenth exercise

--- src/exercises/list/tenth.cpp
+++ src/exercises/list/tenth.cpp
@@ -1,4 +1,5 @@
 #include <exercises/list/tenth.h>
+#include <services/image_handler.h>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
@@ -10,14 +11,35 @@ void TenthExercise::ExecQ1(){
     std::string resource_path = RESOURCE_FILE_DIR;
     std::string image_path = resource_path + "/images/Pedestrian.png";
 
-    cv::samples::findFile(image_path);
-
-    cv::Mat pedestrian_image = cv::imread(image_path, cv::IMREAD_COLOR);
+    cv::Mat pedestrian_image = imagehandler::Import(image_path);
 
     if(pedestrian_image.empty() == false){
-        cv::imshow("Display window", pedestrian_image);
+        imagehandler::Display(pedestrian_image);
 
-        int user_key = cv::waitKey(0);
+        for(int y = 0; y < pedestrian_image.rows; y++){
+            for(int x = 0; x < pedestrian_image.cols; x++){
+                cv::Vec3b &colors = pedestrian_image.at<cv::Vec3b>(y, x);
+
+                int grey_scale_tolerance = 80,
+                    R = colors[0],
+                    G = colors[1],
+                    B = colors[2];
+
+                if(
+                    R <= grey_scale_tolerance &&
+                    G <= grey_scale_tolerance &&
+                    B <= grey_scale_tolerance
+                ){
+                    colors[0] = 4;
+                    colors[1] = 140;
+                    colors[2] = 4;
+                }
+
+                pedestrian_image.at<cv::Vec3b>(cv::Point(x, y)) = colors;
+            }
+        }
+
+        imagehandler::Display(pedestrian_image);
     }else{
         std::cout << "Could not read the image: " << image_path;
     }
